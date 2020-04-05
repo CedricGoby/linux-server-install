27ab3602269728fbe5586c54c4ddabe48816ee2e
Cédric Goby <cedric.goby@inra.fr>
2020-04-05 22:34:42 +0200

    comment

--- linux-post-installation/debian-10-server-post-installation.sh
+++ linux-post-installation/debian-10-server-post-installation.sh
@@ -126,9 +126,9 @@ read choice
 				f_cmd "$_cmd" "$_cmd_text"						
 			fi
 			
-			# Copie de la clé publique dans le fichier ~/.ssh/authorized_keys
+			# Ajout de la clé publique dans le fichier ~/.ssh/authorized_keys
 			_cmd="echo -e "$_public_key" >> $_home/$_user/$_dir_ssh/$_file_authorized_keys"
-			_cmd_text="Copie de la clé publique dans le fichier "$_home/$_user/$_dir_ssh/$_file_authorized_keys"..."
+			_cmd_text="Ajout de la clé publique dans le fichier "$_home/$_user/$_dir_ssh/$_file_authorized_keys"..."
 			f_cmd "$_cmd" "$_cmd_text"
 
 			# Aplication des propriétés et des droits
