fbcf5f08fa9b04fa6110965cf1367e99f6b5da1f
Cédric Goby <cedric.goby@inra.fr>
2020-04-23 21:55:38 +0200

    add install acl

--- linux-post-installation/debian-10-server-post-installation.sh
+++ linux-post-installation/debian-10-server-post-installation.sh
@@ -88,6 +88,7 @@ _cmd="apt-get -y install software-properties-common \
 	apt-transport-https \
 	lsb-release \
 	ca-certificates \
+	acl \
 	curl \
 	gnupg >/dev/null 2>>"$_file_logs""
 _cmd_text="Installation des logiciels pré-requis..."
